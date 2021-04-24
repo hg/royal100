926ee20743c18179c329f11ab0617886a52af72e
Kirill Isakov <is-kir@ya.ru>
2021-04-24 11:15:19 +0600

    webui: выкидываем лишнее барахло

--- chess-ui/src/index.tsx
+++ chess-ui/src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,17 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-Royal100().then((engine) => {
-  engine.addMessageListener((line) => {
-    console.log("line", line);
-  });
-
-  engine.postMessage("uci");
-  engine.postMessage("go depth 1");
-});
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
