hg/royal100

commit b6d68b04000a9a2b2347187939cf209d7031d7de

Support royalty promotion by the engine (#9)

chess-ui/src/game/engine.ts
@@ -34,7 +34,8 @@ interface CastlingAvailability {
 }
 
 export interface Fen {
-  raw: string;
+  from: string;
+  to: string;
   pieces: string;
   color: Color;
   castling: {
@@ -169,7 +170,10 @@ export class Engine {
       try {
         const fen: string = await this.wait(EngineEvent.Fen, 5000);
         if (fen) {
-          return parseFen(fen);
+          return {
+            ...parseFen(fen),
+            from: currentFen,
+          };
         }
       } catch (e) {
         await this.restartEngine();
@@ -322,6 +326,7 @@ export class Engine {
 
   private position(fen: string) {
     assert.ok(this.engine);
+    console.log("sending fen", fen);
     this.engine.postMessage(`position fen ${fen}`);
   }
 

chess-ui/src/game/game.ts
@@ -121,6 +121,7 @@ export class Game {
   private reactionDisposers: IReactionDisposer[] = [];
   private validMoves?: ValidMoves;
   private promotion?: Letter;
+  private coronation?: "K" | "Q";
   private plyIncrement = 0;
   private showAnalysis = true;
   @observable private opponent = OpponentType.Computer;
@@ -498,7 +499,7 @@ export class Game {
       this.promotion = await this.checkPawnPromotions(orig, dest);
     }
 
-    await this.setFen(this.fen.raw, [`${orig}${dest}${this.promotion || ""}`]);
+    await this.setFen(this.fen.to, [`${orig}${dest}${this.promotion || ""}`]);
 
     this.promotion = undefined;
 
@@ -513,15 +514,36 @@ export class Game {
       captured: capturedPiece,
       piece: this.ground.state.pieces[dest]!,
       color: opposite(this.turnColor),
-      fen: this.fen.raw,
+      fen: this.fen.to,
       mate: false,
       check: false,
     };
 
-    this.previousFen = this.fen.raw;
+    this.previousFen = this.fen.to;
 
     if (capturedPiece) {
       await this.checkRoyaltyPromotions(capturedPiece);
+    }
+
+    if (this.isOpponentAComputer && this.turnColor === this.myColor) {
+      if (this.coronation === "Q") {
+        const princess = this.locatePiece(
+          pieces.princess,
+          opposite(this.turnColor)
+        );
+        if (princess) {
+          this.ground.setPieces({
+            [princess]: {
+              role: pieces.queen,
+              color: opposite(this.turnColor),
+              promoted: true,
+            },
+          });
+          this.fen.pieces = fixBoardFen(this.ground.getFen());
+          this.fen.to = fenToString(this.fen);
+        }
+        this.coronation = undefined;
+      }
     }
 
     if (this.clocks.used) {
@@ -579,7 +601,7 @@ export class Game {
         },
       });
       this.fen.pieces = fixBoardFen(this.ground.getFen());
-      this.fen.raw = fenToString(this.fen);
+      this.fen.to = fenToString(this.fen);
     }
   }
 
@@ -784,7 +806,7 @@ export class Game {
   async getHint(): Promise<BestMove | undefined> {
     this.setThinking(true);
     try {
-      const move = await this.engine.calculateMove(this.fen.raw);
+      const move = await this.engine.calculateMove(this.fen.to);
       if (move) {
         this.ground.selectSquare(move.from);
         this.ground.setShapes([
@@ -811,7 +833,7 @@ export class Game {
 
     this.setThinking(true);
     try {
-      await this.engine.calculateMove(this.fen.raw);
+      await this.engine.calculateMove(this.fen.to);
     } finally {
       this.setThinking(false);
     }
@@ -844,18 +866,27 @@ export class Game {
   }
 
   private async makeOpponentMove() {
-    if (this.isOpponentAComputer && this.turnColor !== this.myColor) {
-      this.setThinking(true);
-      try {
-        const move = await this.engine.calculateMove(this.fen.raw);
-        if (move) {
-          this.promotion = move.promotion;
-          const captured = this.ground.state.pieces[move.to];
-          await this.onMove(move.from, move.to, captured);
-        }
-      } finally {
-        this.setThinking(false);
+    if (this.isOpponentHuman || this.turnColor === this.myColor) {
+      return;
+    }
+
+    const lastMove = this.moves[this.moves.length - 1];
+
+    const fen = lastMove
+      ? `${this.fen.from} moves ${lastMove.from}${lastMove.to}`
+      : this.fen.to;
+
+    this.setThinking(true);
+    try {
+      const move = await this.engine.calculateMove(fen);
+      if (move) {
+        this.coronation = move.coronation;
+        this.promotion = move.promotion;
+        const captured = this.ground.state.pieces[move.to];
+        await this.onMove(move.from, move.to, captured);
       }
+    } finally {
+      this.setThinking(false);
     }
   }
 

chess-ui/src/utils/chess.ts
@@ -73,7 +73,8 @@ export function parseFen(fen: string): Fen {
     fen.split(reWs);
 
   return {
-    raw: fen,
+    from: "",
+    to: fen,
     pieces: compressPieces(pieces),
     color: color === "w" ? "white" : "black",
     castling: {
@@ -158,20 +159,22 @@ export interface BestMove {
   from: Key;
   to: Key;
   promotion?: Letter;
+  coronation?: "K" | "Q";
 }
 
-const reBestMove = /\bbestmove ([a-j]\d+)([a-j]\d+)(\w?)\b/;
+const reBestMove = /\bbestmove ([KQ]?)([a-j]\d+)([a-j]\d+)(\w?)\b/;
 
 export function parseBestMove(data: string): BestMove | undefined {
   const matchMove = data.match(reBestMove);
   if (!matchMove) {
     return undefined;
   }
-  const [, from, to, promotion] = matchMove;
+  const [, coronation, from, to, promotion] = matchMove;
   return {
     from: enginePositionToBoard(from),
     to: enginePositionToBoard(to),
-    promotion: promotion as Letter,
+    promotion: promotion as BestMove["promotion"],
+    coronation: coronation as BestMove["coronation"],
   };
 }
 

commit 741b94be6cd815ecef165edf15c253c2e42b220c

Fix two crashes introduced in b6d68b04000a9a2b234

chess-ui/src/game/engine.ts
@@ -326,8 +326,9 @@ export class Engine {
 
   private position(fen: string) {
     assert.ok(this.engine);
-    console.log("sending fen", fen);
-    this.engine.postMessage(`position fen ${fen}`);
+    const normalized = fen.replaceAll(":", "10");
+    console.log("sending fen", normalized);
+    this.engine.postMessage(`position fen ${normalized}`);
   }
 
   private go() {

chess-ui/src/pages/ChessBoard/MoveHistory/index.tsx
@@ -98,7 +98,7 @@ const TableRow: FC<RowProps> = ({ move, num, setMove, isLast, isCurrent }) => {
         )}
       </td>
       <td>
-        <MovePiece role={move.piece.role} color={move.color} />
+        <MovePiece role={move.piece?.role} color={move.color} />
       </td>
       <td>{formatMove(move.from)}</td>
       <td>{formatMove(move.to)}</td>